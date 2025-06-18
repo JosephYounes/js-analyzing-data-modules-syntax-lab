b7205993aa624ba7c5253230695905d335b440c4
JosephYounes <joseph.younes52@hotmail.com>
2025-06-18 14:36:33 -0400

    this is my analyzing data lab

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const Datejs = require('datejs');
 
-
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  for (const userArray of args) {
+    combinedObject.users.push(...userArray);
+  }
+  // Use Datejs for formatting the date
+  const today = new Date();
+  const formatted = today.toString('M/d/yyyy');
+  combinedObject.merge_date = formatted;
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
